b6ea55e5e8eb938666315e9d8ed8ad3c62ff305d
arian154639-cyber <arian154639@gmail.com>
2026-05-13 18:18:28 -0400

    Add files via upload

--- recursion.py
+++ recursion.py
@@ -12,17 +12,13 @@ def gcd(a, b):
 def compareTo(s1, s2):
     if s1 == "" and s2 == "":
         return 0
-
-    if s1 == "":
+    elif s1 == "":
         return -1
-
-    if s2 == "":
+    elif s2 == "":
         return 1
-
-    if s1[0] < s2[0]:
+    elif s1[0] < s2[0]:
         return -1
-
-    if s1[0] > s2[0]:
+    elif s1[0] > s2[0]:
         return 1
 
     return compareTo(s1[1:], s2[1:])
